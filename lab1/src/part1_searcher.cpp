cc2c5b9097517938bfe3abff1d1928855027009b
Sujata Kar <161945164+Sujatakar-04@users.noreply.github.com>
2024-08-13 17:31:52 +0530

    Add files via upload

---
+++ lab1/src/part1_searcher.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <unistd.h>
+#include <signal.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	if(argc != 5)
+	{
+		cout <<"usage: ./partitioner.out <path-to-file> <pattern> <search-start-position> <search-end-position>\nprovided arguments:\n";
+		for(int i = 0; i < argc; i++)
+			cout << argv[i] << "\n";
+		return -1;
+	}
+	
+	char *file_to_search_in = argv[1];
+	char *pattern_to_search_for = argv[2];
+	int search_start_position = atoi(argv[3]);
+	int search_end_position = atoi(argv[4]);
+
+	//TODO
+	int pattern_len = strlen(pattern_to_search_for);
+	ifstream file(file_to_search_in);
+	char c;
+	int i = search_start_position;
+	int j = 0;
+	while(file.get(c) && i<=search_end_position){
+		if(c==pattern_to_search_for[j]){
+			j++;
+		//    i++;
+			if(j==pattern_len){
+				cout << "[" << getpid() << "]" << "found at" << (i-pattern_len+1);
+				return 1;
+			}
+		}
+		else{
+			j=0;
+			if(c==pattern_to_search_for[j]){
+			j++;
+		}
+		i++;
+	}
+	}
+	cout << "[-1] didn't find\n";
+	return 0;
+}
