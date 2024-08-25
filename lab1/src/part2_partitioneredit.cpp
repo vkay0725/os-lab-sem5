7d3626dbb437efbdcbb7a30b1a07a58dec7baf13
vkay0725 <153687089+vkay0725@users.noreply.github.com>
2024-08-25 19:20:07 +0530

    Update part2_partitioneredit.cpp
    added correct print statements

--- lab1/src/part2_partitioneredit.cpp
+++ lab1/src/part2_partitioneredit.cpp
@@ -20,39 +20,54 @@ int main(int argc, char **argv) {
 
     int chunk_size = search_end_position - search_start_position + 1;
 
-    cout << "Process " << getpid() << " started, searching in range " << search_start_position << " - " << search_end_position << "\n";
+    cout << "[" << getpid() << "] start position = " << search_start_position << " ; end position = " << search_end_position << "\n";
 
     if (chunk_size > max_chunk_size) {
+      
         int mid = search_start_position + (chunk_size / 2);
 
         pid_t left_pid = fork();
         if (left_pid == 0) {
-            char mid_str[20]; // child process for left half
+            char mid_str[20];
             snprintf(mid_str, sizeof(mid_str), "%d", mid);
+            cout << "[" << getppid() << "] forked left child " << getpid() << "\n";
             execl("./part2_partitioner.out", "part2_partitioner.out", file_to_search_in, pattern_to_search_for, argv[3], mid_str, to_string(max_chunk_size).c_str(), NULL);
             cerr << "Failed to execute left partitioner.\n";
             return -1;
-        } else {
+        } else if (left_pid > 0) {
             pid_t right_pid = fork();
-            if (right_pid == 0) { //child process for the right half
+            if (right_pid == 0) {
                 char mid_str[20];
-                snprintf(mid_str, sizeof(mid_str), "%d", mid + 1); // +1 to avoid overlapping
+                snprintf(mid_str, sizeof(mid_str), "%d", mid + 1);
+                cout << "[" << getppid() << "] forked right child " << getpid() << "\n";
                 execl("./part2_partitioner.out", "part2_partitioner.out", file_to_search_in, pattern_to_search_for, mid_str, argv[4], to_string(max_chunk_size).c_str(), NULL);
-                cerr << "Failed to execute right partitioner.\n"; //c_str(): provides  const char* ptr to internal char array of the string; suitable for execl
+                cerr << "Failed to execute right partitioner.\n";
                 return -1;
-            } else {
-                int status; //parent process waits for both children to finish
+            } else if (right_pid > 0) {
+                int status;
                 waitpid(left_pid, &status, 0);
+                cout << "[" << getpid() << "] left child " << left_pid << " returned\n";
                 waitpid(right_pid, &status, 0);
+                cout << "[" << getpid() << "] right child " << right_pid << " returned\n";
+            } else {
+                cerr << "Failed to fork right child.\n";
+                return -1;
             }
+        } else {
+            cerr << "Failed to fork left child.\n";
+            return -1;
         }
     } else {
-        char start_str[20]; //if chunk size is within limit, perform the search
+        char start_str[20];
         char end_str[20];
-        snprintf(start_str, sizeof(start_str), "%d", search_start_position); // convert int to string
+        snprintf(start_str, sizeof(start_str), "%d", search_start_position);
         snprintf(end_str, sizeof(end_str), "%d", search_end_position);
+
+        cout << "[" << getpid() << "] found at\n";
+
         execl("./part2_searcher.out", "part2_searcher.out", file_to_search_in, pattern_to_search_for, start_str, end_str, NULL);
-        cerr << "Failed to execute searcher.\n";
+
+        cout << "[" << getpid() << "] didn't find\n";  
         return -1;
     }
 
