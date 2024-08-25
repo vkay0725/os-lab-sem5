ee29f35b1bd7862d71e140dded48afbf84f5fa4e
vkay0725 <153687089+vkay0725@users.noreply.github.com>
2024-08-25 21:57:39 +0530

    Update part2_partitioneredit.cpp

--- lab1/src/part2_partitioneredit.cpp
+++ lab1/src/part2_partitioneredit.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <cerrno>
 
 using namespace std;
 
@@ -23,7 +24,6 @@ int main(int argc, char **argv) {
     cout << "[" << getpid() << "] start position = " << search_start_position << " ; end position = " << search_end_position << "\n";
 
     if (chunk_size > max_chunk_size) {
-      
         int mid = search_start_position + (chunk_size / 2);
 
         pid_t left_pid = fork();
@@ -44,10 +44,22 @@ int main(int argc, char **argv) {
                 cerr << "Failed to execute right partitioner.\n";
                 return -1;
             } else if (right_pid > 0) {
+                // Parent process logs the forked searcher PID
+                cout << "[" << getpid() << "] forked left child " << left_pid << "\n";
+                cout << "[" << getpid() << "] forked right child " << right_pid << "\n";
+                
+                // Wait for the partitioner children processes to complete
                 int status;
-                waitpid(left_pid, &status, 0);
+                if (waitpid(left_pid, &status, 0) == -1) {
+                    cerr << "Error waiting for left child process: " << strerror(errno) << "\n";
+                    exit(1);
+                }
                 cout << "[" << getpid() << "] left child " << left_pid << " returned\n";
-                waitpid(right_pid, &status, 0);
+
+                if (waitpid(right_pid, &status, 0) == -1) {
+                    cerr << "Error waiting for right child process: " << strerror(errno) << "\n";
+                    exit(1);
+                }
                 cout << "[" << getpid() << "] right child " << right_pid << " returned\n";
             } else {
                 cerr << "Failed to fork right child.\n";
@@ -63,12 +75,27 @@ int main(int argc, char **argv) {
         snprintf(start_str, sizeof(start_str), "%d", search_start_position);
         snprintf(end_str, sizeof(end_str), "%d", search_end_position);
 
-        cout << "[" << getpid() << "] found at\n";
-
-        execl("./part2_searcher.out", "part2_searcher.out", file_to_search_in, pattern_to_search_for, start_str, end_str, NULL);
-
-        cout << "[" << getpid() << "] didn't find\n";  
-        return -1;
+        pid_t searcher_pid = fork();
+        if (searcher_pid == 0) {
+            cout << "[" << getppid() << "] forked searcher child " << getpid() << "\n";
+            execl("./part2_searcher.out", "part2_searcher.out", file_to_search_in, pattern_to_search_for, start_str, end_str, NULL);
+            cerr << "Failed to execute searcher.\n";
+            return -1;
+        } else if (searcher_pid > 0) {
+            // Parent process logs the forked searcher PID
+            cout << "[" << getpid() << "] forked searcher child " << searcher_pid << "\n";
+            
+            // Wait for the searcher child process to complete
+            int status;
+            if (waitpid(searcher_pid, &status, 0) == -1) {
+                cerr << "Error waiting for searcher child process: " << strerror(errno) << "\n";
+                exit(1);
+            }
+            cout << "[" << getpid() << "] searcher child " << searcher_pid << " returned\n";
+        } else {
+            cerr << "Failed to fork searcher child.\n";
+            return -1;
+        }
     }
 
     return 0;
